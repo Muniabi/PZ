aad4952c225e61c7ee505d0bd4d443b76414c479
Muniabi <volansinfo@yandex.ru>
2022-11-12 16:59:45 +0300

    pz
    pz

--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,1 +1,18 @@
-#Не могу найти папку в которую сохранил, загружу чуть позже
+import random
+
+N = random.randrange(1,100)
+
+print('N = ', N)
+
+K = 1
+S = 1
+
+while S <= N:
+    K += 1
+    S += K
+    print("K = {0}, S = {1}". format(K,S))
+
+S -= K
+K -= 1
+print()
+print("K = {0}, S = {1}". format(K,S))
