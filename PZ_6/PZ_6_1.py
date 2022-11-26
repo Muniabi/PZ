97f7ab6afdd4ad97182d699b75044e54166122a4
Muniabi <volansinfo@yandex.ru>
2022-11-26 12:36:47 +0300

    Add PZ_6 (отчеты загружу позже))
    т.к. у меня не добавляются картинки

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,17 @@
+a = []
+
+N = int(input("N:"))
+for i in range(N):
+    a.append(int(input(" : ")))
+
+Temp = (a[0] > 0)
+i = 1
+
+while ((i < N) and (Temp != (a[i] > 0))):
+    Temp = (a[i] > 0)
+    i += 1
+
+if i == N:
+    print('0')
+else:
+    print(i + 1)
