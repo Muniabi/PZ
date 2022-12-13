34196d44649935c7b9982bd031dc98fc169641ee
Muniabi <96337804+Muniabi@users.noreply.github.com>
2022-12-13 11:31:18 +0300

    Добавил комментарии

--- PZ_7/PZ_7_1.py
+++ PZ_7/PZ_7_1.py
@@ -1,5 +1,6 @@
 import random
-
+# Получаем рандомное число в диапазоне который задан. Для примера K1 будет равен рандомному числу от 1 до 21,
+# N1 будет равен рандомному числу в диапазоне от 1 до рандомного числа K1 к которому добавили 
 K1 = random.randrange(1,21)
 N1 = random.randrange(1,K1+1)
 S1 = input()
@@ -7,10 +8,10 @@ K2 = random.randrange(1,21)
 N2 = random.randrange(1,K2+1)
 S2 = input()
 
+#выводим строки и их длину
 print("N1:",N1)
 print("K1:",K1)
 print("S1:",S1)
-
 S1_length = len(S1)
 print("Длина S1:",S1_length)
 
@@ -22,6 +23,7 @@ print("S2:",S2)
 S2_length = len(S2)
 print("Длина S2:",S2_length)
 
+# работа со срезами по индексам
 S1_new = S1[:N1]
 S1_new_length = len(S1_new)
 print("\nНовая строка S1:",S1_new)
